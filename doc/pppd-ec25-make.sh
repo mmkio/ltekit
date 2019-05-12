3e63bf30c85ade51767ebcc77f4d5e8ddfc48c10
mmkio <50529877+mmkio@users.noreply.github.com>
2019-05-12 21:09:34 +0900

    Update pppd-ec25-make.sh

--- doc/pppd-ec25-make.sh
+++ doc/pppd-ec25-make.sh
@@ -35,9 +35,9 @@ echo "creating script file : /etc/ppp/peers/lte"
 echo "
 /dev/ttyUSB3 115200
 # The chat script, customize your APN in this file
-connect 'chat -s -v -f /etc/chatscripts/quectel-chat-connect -T $1'
+connect 'chat -s -v -f /etc/ppp/peers/quectel-chat-connect -T $1'
 # The close script
-disconnect 'chat -s -v -f /etc/chatscripts/quectel-chat-disconnect'
+disconnect 'chat -s -v -f /etc/ppp/peers/quectel-chat-disconnect'
 # Hide password in debug messages
 hide-password
 # The phone is not required to authenticate
